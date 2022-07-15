ef7f97247f9b6b605904d5808eef5a65f5a0e201
stelaukin <stelaukin@outlook.com>
2022-07-15 16:23:53 +1000

    trying vue and js but doesn't work

---
+++ task03.js
@@ -0,0 +1,11 @@
+let vue = new vue({
+    el: "#app",
+    data: {
+        todos: [
+            {title: 'cheese', checked: true},
+            {title: 'salami', checked: true},
+            {title: 'dip', checked: true},
+            {title: 'biscuit', checked: true},
+        ],
+    },
+});
