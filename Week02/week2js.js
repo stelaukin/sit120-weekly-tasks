503e2fb8d2c5c5ac728b4642049092d02e180e4c
stelaukin <stelaukin@outlook.com>
2022-07-19 12:20:59 +1000

    week2 adjustments

---
+++ Week02/week2js.js
@@ -0,0 +1,11 @@
+let vue = new Vue({
+    el: "#app",
+    data: {
+        todos: [
+            {title: 'Evasion', checked: true},
+            {title: 'Elite Kicking', checked: true},
+            {title: 'Clearances', checked: true},
+            {title: 'Name sounds like an alcopop drink', checked: true},
+        ],
+    },
+});
