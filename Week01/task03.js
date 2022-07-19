72a8468fbfb72e65d477dbab0d4b2ce7d9fea31e
stelaukin <stelaukin@outlook.com>
2022-07-19 11:33:26 +1000

    nav bar

--- Week01/task03.js
+++ Week01/task03.js
@@ -2,10 +2,10 @@ let vue = new Vue({
     el: "#app",
     data: {
         todos: [
-            {title: 'cheese', checked: true},
-            {title: 'salami', checked: true},
-            {title: 'dip', checked: true},
-            {title: 'biscuit', checked: true},
+            {title: 'Evasion', checked: true},
+            {title: 'Elite Kicking', checked: true},
+            {title: 'Clearances', checked: true},
+            {title: 'Name sounds like an alcopop drink', checked: true},
         ],
     },
 });
