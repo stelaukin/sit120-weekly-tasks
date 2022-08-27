cc9aed2b5c517253753ba58e4a938c0191f48c66
stelaukin <stelaukin@outlook.com>
2022-08-28 08:51:42 +1000

    sunday

--- Week03/StringMethods.js
+++ Week03/StringMethods.js
@@ -21,3 +21,37 @@ else {
     console.log('It is lower!')
 }
 
+
+let x = 'x';
+let y = 'y';
+
+if (x < y) {
+    console.log(x + ' is less than ' + y);
+} else if (x > y){
+    console.log(x + ' is greater than ' + y);
+} else {
+    console.log(x + ' and ' + y + ' are equal');
+}
+
+
+let longString =
+    'Thanks Basil. I just want to give you a bit of an insight into the Saturday afternoon team. ' +
+    'We’re a bunch of unique individuals – Malthouse, Harley, Zempilas, McLachlan and myself. ' +
+    'We’re gonna engage with our minds and our hearts. We’re gonna exchange ideas, thoughts and even our energy. ' +
+    'And to speak from your heart you need to have courage. You need to be honest and authentic. ' +
+    'We’re not always gonna get it right. And sometimes, you’re not going to agree with us. ' +
+    'But that’s okay, because just like you we care. Just like you, we’re passionate about the game. ' +
+    'So if you want to come along for the ride, join us on Saturday arvos, because we just love the footy.';
+
+console.log(longString);
+
+longString = 
+    'Thanks Basil. I just want to give you a bit of an insight into the Saturday afternoon team.\
+    We’re a bunch of unique individuals – Malthouse, Harley, Zempilas, McLachlan and myself.\
+    We’re gonna engage with our minds and our hearts. We’re gonna exchange ideas, thoughts and even our energy. \
+    And to speak from your heart you need to have courage. You need to be honest and authentic. \
+    We’re not always gonna get it right. And sometimes, you’re not going to agree with us. \
+    But that’s okay, because just like you we care. Just like you, we’re passionate about the game. \
+    So if you want to come along for the ride, join us on Saturday arvos, because we just love the footy.';
+
+console.log(longString);
