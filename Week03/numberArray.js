97a874cba990782c4c4d9078abf7b4b7dc92f496
stelaukin <stelaukin@outlook.com>
2022-08-27 19:44:46 +1000

    maybe

---
+++ Week03/numberArray.js
@@ -0,0 +1,11 @@
+// JavaScript Number and Array Methods
+
+let cheese = ['Gouda', 'Edam', 'Havarti', 'Brie'];
+
+console.log(cheese);
+
+console.log(cheese.find((cheese) => cheese === 'Edam'));
+console.log(cheese.find((cheese) => cheese === 'Swiss'));
+
+
+
---
+++ Week03/task3.js
@@ -0,0 +1,4 @@
+
+const now = new Date(Date.now);
+
+console.log
