45f35a993dd4a3e824ed541bc8c6338d9cb411a6
stelaukin <stelaukin@outlook.com>
2022-08-27 21:53:12 +1000

    pushing and stuff

--- Week03/task3.js
+++ Week03/task3.js
@@ -1,4 +1,12 @@
 
-const now = new Date(Date.now);
+const now = new Date(Date.now());
 
-console.log
+console.log('the current time is');
+console.log(now.toDateString());
+
+const today = new Date();
+// const birthday = new Date('August 8, 1986 20:00:00');
+
+const birthday = new Date('1986-08-08T08:00:00');
+console.log(birthday);
+
