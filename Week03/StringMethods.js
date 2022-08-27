3abc23a48854d603b7c1669a0c4f1729cc696c4b
stelaukin <stelaukin@outlook.com>
2022-08-27 19:23:36 +1000

    working like a dog

---
+++ Week03/StringMethods.js
@@ -0,0 +1,8 @@
+/* Pass Task 1 – JavaScript String Methods
+Instruction: String methods help us to work with strings. Visit our Unit site or
+https://www.w3schools.com/js/ for more information on JS details. Please see week 3 task
+1 video tutorial for more detail. */
+
+var helloWorld = 'Hello World!';
+
+console.log(helloWorld);
