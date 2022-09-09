e3c6fd74abe9492ebdd7353f4065f11a551b744f
stelaukin <stelaukin@outlook.com>
2022-09-09 12:51:58 +1000

    update

---
+++ Week06/main.js
@@ -0,0 +1,12 @@
+/*Pass Task 1 – Using v-model directive for Handling User Input (Form Input Bindings)
+Instruction: When user inserts some values in form input, v-model directive can be used to
+sync the state between the form input element and the corresponding state in the
+JavaScript. V-model directive helps in automatically accommodate that value in local data.
+Let’s try to implement this using v-model. Please see Form Input Bindings — Vue.js
+(vuejs.org) for detail. For hint, please see the week 6 task 1 video tutorial.
+Task to submit: Create a Vue application with two different types of input e.g., password
+and email. Below the input tag you must display the value inside input tag. For example, a
+<p> tag below the input tag should display the input value of password input. So, when you
+change the password in input tag, then the <p> tag content should also change and display
+the password. Write 1-2 sentences reflection.*/
+
